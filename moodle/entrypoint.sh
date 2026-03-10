3d6e6221aa60901a6e2dc0a25f24382b06adc0c8
Ian Stride <ianstride@gmail.com>
2026-03-10 23:07:26 +0000

    Tidy

--- moodle/entrypoint.sh
+++ moodle/entrypoint.sh
@@ -13,7 +13,14 @@ cat /opt/idems/config.php.template | envsubst "$(sed -e 's/^/$/' /opt/idems/conf
 echo "Configuration file (config.php) created"
 
 set +e
-php admin/cli/install_database.php --agree-license --adminuser=${MOODLE_ADMIN_USER} --adminpass=${MOODLE_ADMIN_PASS} --adminemail=${MOODLE_ADMIN_EMAIL} --fullname="${MOODLE_FULL_NAME}" --shortname="${MOODLE_SHORT_NAME}" --supportemail=${MOODLE_SUPPORT_EMAIL}
+php admin/cli/install_database.php \
+    --agree-license \
+    --adminuser=${MOODLE_ADMIN_USER} \
+    --adminpass=${MOODLE_ADMIN_PASS} \
+    --adminemail=${MOODLE_ADMIN_EMAIL} \
+    --fullname="${MOODLE_FULL_NAME}" \
+    --shortname="${MOODLE_SHORT_NAME}" \
+    --supportemail=${MOODLE_SUPPORT_EMAIL}
 set -e
 echo "Database setup completed"
 
